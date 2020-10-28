87ea74e4109f1f8dee6ac3cc63e65c597aa6651c
MrDxD-SoulAdmin <68185057+MrDxD-SoulAdmin@users.noreply.github.com>
2020-10-28 19:31:01 +0100

    remove pass

--- LogMain/login.js
+++ LogMain/login.js
@@ -1,36 +1,3 @@
-console.log("nono");
-const users = [{
-        name: "Soul",
-        Pass: "NetAdmin"
-    },
-    {
-        name: "Selti",
-        Pass: "NetUser001"
-    }, {
-        name: "N0rris",
-        Pass: "wizynwstc"
-    },
-    {
-        name: "Degubi",
-        Pass: "NetUser003"
-    }, {
-        name: "Adam",
-        Pass: "NetUser004"
-    }, {
-        name: "Ati",
-        Pass: "NetUser005"
-    }, {
-        name: "Marci",
-        Pass: "NetUser006"
-    }, {
-        name: "Don_Nyuff",
-        Pass: "Szben1997"
-    }, {
-        name: "Roli",
-        Pass: "Kamujelszo02"
-    },
-];
-
 function login() {
     var IU = document.getElementById("INU").value;
     var IP = document.getElementById("INP").value;
@@ -51,7 +18,6 @@ function login() {
 }
 
 function enter(event) {
-    console.log("asd");
     if (event.keyCode === 13) {
         event.preventDefault();
         login();
